447ad7d5108ba69fdc375169c66ee8b77ce62c4b
Ronniedevloper <ronniedeveloper256@gmail.com>
2022-09-04 01:36:05 +0300

    Updated the follow me links to my profiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,31 @@
-import { FaCopyright,FaGithub,FaFacebook, FaInstagram,FaUserNinja, FaSearchLocation, FaGlobe } from 'react-icons/fa';
+import { FaCopyright,FaGithub,FaFacebook, FaInstagram,FaTwitter } from 'react-icons/fa';
 const Footer = () => {
     return (
         <div className="footer">
             <div className="copyright">
-                <FaCopyright className='fa-copyright'/> 
-                <h1>Copyright</h1>
+                <div className='copyright-icon'>
+                    <h1>Copyright</h1>
+                    <FaCopyright className='fa-copyright'/>
+                </div>
                 <p>All Right Reserved</p>
                 <p>Product of 2022</p>
             </div>
             <div className='follow-me'>
                 <h1>Follow me</h1>
                 <div className='follow-details'>      
-                    <p><FaGithub color='indigo'/> Ronnie_Developer</p>
-                    <p><FaFacebook color='blue'/> Ronnie_Developer</p>
-                    <p><FaInstagram color='violet' title='Fuck Me'/> Ronnie_Developer</p>
+                    <a href='https://github.com/Ronniedeveloper'>
+                        <FaGithub color='indigo'className='github'/>
+                    </a>
+                    <a href='https://www.facebook.com/profile.php?id=100075241205617'>
+                        <FaFacebook color='blue' className='fb'/>
+                    </a>
+                    <a href='https://www.instagram.com/p/ChRwgYtOUHd/?igshid=YmMyMTA2M2Y='>
+                        <FaInstagram color='violet' className='instagram'/>
+                    </a>
+                    <a href='https://twitter.com/RonnieDev256?t=5Hey-xti7SYiCypHh0G3tg&s=09'>
+                        <FaTwitter color='blue' className='twitter'/>
+                    </a>
                 </div>
-            </div>
-            <div className="footer-content">
-                <article>
-                    <h1>Contact Me</h1>
-                    <p><FaUserNinja color='black'/>: Ronnie Developer</p>
-                    <p><FaSearchLocation color='orange'/>: ronniedeveloper256@gmail.com</p>
-                    <p><FaGlobe color='blue'/>: ReactJs</p>
-                </article>
             </div>
         </div>
     );
