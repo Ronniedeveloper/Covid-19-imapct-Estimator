988abf71bbe92a01bc4a570bc9107ca3c7ee8394
Ronniedevloper <ronniedeveloper256@gmail.com>
2023-04-20 00:41:58 +0300

    added one project to my portifolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import { FaCopyright,FaGithub,FaFacebook, FaInstagram,FaTwitter,FaLinkedin } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <div className="footer">
             <div className="copyright">
                 <div className='copyright-icon'>
-                    <h1>Copyright</h1>
                     <FaCopyright className='fa-copyright'/>
+                    <h1>Copyright</h1>
                 </div>
                 <p>All Right Reserved</p>
                 <p>Product of 2022</p>
@@ -13,21 +15,21 @@ const Footer = () => {
             <div className='follow-me'>
                 <h1>Follow me</h1>
                 <div className='follow-details'>      
-                    <a href='https://github.com/Ronniedeveloper'>
+                    <Link to='https://github.com/Ronniedeveloper'>
                         <FaGithub color='indigo'className='github'/>
-                    </a>
-                    <a href='https://www.facebook.com/profile.php?id=100075241205617'>
+                    </Link>
+                    <Link to='https://www.facebook.com/profile.php?id=100075241205617'>
                         <FaFacebook color='blue' className='fb'/>
-                    </a>
-                    <a href='https://www.instagram.com/p/ChRwgYtOUHd/?igshid=YmMyMTA2M2Y='>
+                    </Link>
+                    <Link to='https://www.instagram.com/p/ChRwgYtOUHd/?igshid=YmMyMTA2M2Y='>
                         <FaInstagram color='violet' className='instagram'/>
-                    </a>
-                    <a href='https://twitter.com/RonnieDev256?t=5Hey-xti7SYiCypHh0G3tg&s=09'>
+                    </Link>
+                    <Link to='https://twitter.com/RonnieDev256?t=5Hey-xti7SYiCypHh0G3tg&s=09'>
                         <FaTwitter color='blue' className='twitter'/>
-                    </a>
-                    <a href='https://www.linkedin.com/in/ronnie-developer-7939a1248/'>
+                    </Link>
+                    <Link to='https://www.linkedin.com/in/ronnie-developer-7939a1248/'>
                         <FaLinkedin color='blue' className='twitter'/>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/components/LatestWork.js
+++ src/components/LatestWork.js
@@ -1,8 +1,20 @@
+import work from '../images/latestwork.jpg'
+import forms from '../images/login_forms.jpg'
+
 const MyLatestWork = () => {
     return (
         <div className="latest-work">
             <h1>My Latest Work</h1>
-            <div className="image"></div>
+            <div className="image">
+                <div className='about-work'>
+                    <h3>Personal Portifolio</h3>
+                    <button className='source-code'>Source Code</button>
+                </div>
+                <div className='images'>
+                    <img src={work} width={300} height={150} alt=""/>
+                    <img src={forms} width={300} height={150} alt=""/>
+                </div>
+            </div>
         </div>
     );
 }
